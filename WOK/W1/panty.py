6bebb0229f3ad597a782e8b10cd8d0d0ee98f919
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-19 17:53:39 +0000

    🫦

--- WOK/W1/panty.py
+++ WOK/W1/panty.py
@@ -6,16 +6,25 @@ from src.banner.b2 import pussy
 panty = typer.Typer(name="AssSniff")
 
 
+# Geenral Command Test 1
 @panty.command()
 def drive():
     pussy()
     print("hello")
 
 
+# General command Test 2
 @panty.command()
 def drive2():
     pussy()
     print("hello Sniff")
+
+
+# Scraper Commad Here
+@panty.command()
+def scrape():
+    """Command executes scraping pussy"""
+    print("[red] Scraping Panty [/red]")
 
 
 if __name__ == "__main__":

8544b6589dabe2eb5ed9a68ea7f4afa82e15b785
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-19 17:41:50 +0000

    🫦

--- WOK/W1/panty.py
+++ WOK/W1/panty.py
@@ -1,12 +1,21 @@
-# Main entry point for the typer cli 
-import typer 
+# Main entry point for the typer cli
+import typer
 from rich import print
+from src.banner.b1 import pussy
 
 panty = typer.Typer(name="AssSniff")
 
+
 @panty.command()
 def drive():
+    pussy()
     print("hello")
+
+
+@panty.command()
+def drive2():
+    print("hello Sniff")
+
 
 if __name__ == "__main__":
     panty()

6a64ffc3a7b8e66cf07ac89250a96380a16b725b
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-19 18:25:21 +0000

    🫦

--- WOK/W1/panty.py
+++ WOK/W1/panty.py
@@ -2,8 +2,16 @@
 import typer
 from rich import print
 from src.banner.b2 import pussy
+from src.sc1.s1 import s1
 
 panty = typer.Typer(name="AssSniff")
+
+
+@panty.callback()
+def main(ctx: typer.Context):
+    print("[red]AssSniff[/red]")
+    if ctx.invoked_subcommand is None:
+        typer.echo("No subcommand specified. Use --help for more information.")
 
 
 # Geenral Command Test 1
@@ -25,6 +33,7 @@ def drive2():
 def scrape():
     """Command executes scraping pussy"""
     print("[red] Scraping Panty [/red]")
+    s1()
 
 
 if __name__ == "__main__":
---
+++ WOK/W1/src/sc1/s1.py
@@ -0,0 +1,5 @@
+from rich import print as print
+
+
+def s1():
+    print("[green]hello[/green]")

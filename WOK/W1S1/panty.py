bb7ef7484a5ad58bf0f4098878448720fb263b70
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-20 14:31:56 +0000

    🫦

--- WOK/W1S1/panty.py
+++ WOK/W1S1/panty.py
@@ -1,10 +1,14 @@
 # Main Entry point
-
+from src.S1.s1 import s1s
+import asyncio
 from rich.traceback import install
+
 install(show_locals=True)
+
 
 def main():
     print("Hello from w1s1!")
+    asyncio.run(s1s())
 
 
 if __name__ == "__main__":

b6b5a768d17b3d1cb48548121b9f136c43cab26c
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-20 17:48:34 +0000

    🫦

--- WOK/W1S1/src/S1/s1.py
+++ WOK/W1S1/src/S1/s1.py
@@ -8,11 +8,11 @@ from rich.traceback import install
 
 install(show_locals=True)
 
-# Playwright stealth settings
+# Playwright Stealth settings
 STEALTH_SETTINGS = {
     "bypassCSP": True,
     "stealth": True,
-    "headless": False,
+    "headless": True,  # Set headless mode to True
     "slowMo": 50,
     "devtools": False,
 }
@@ -37,52 +37,50 @@ logging.basicConfig(
 logger = logging.getLogger("rich")
 
 
-# Function to generate a filename based on the URL and extension
 def get_filename(url, extension):
     domain = urlparse(url).netloc
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     return f"{domain}_{timestamp}.{extension}"
 
 
-# Function to set up the browser
 async def setup_browser():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(
-            headless=False, args=["--no-sandbox", "--disable-setuid-sandbox"]
-        )
-        context = await browser.new_context(
-            user_agent=USER_AGENT,
-            locale="de-DE",
-            geolocation={"longitude": -43.1729, "latitude": -22.9068},
-            permissions=["geolocation"],
-            record_video_dir="panties/",
-            record_video_size={"width": 1280, "height": 720},
-        )
-        page = await context.new_page()
-        await page.evaluate_on_new_document(
-            """
-            Object.defineProperty(navigator, 'webdriver', {
-              get: () => false,
-            });
-            """
-        )
-        await page.evaluate_on_new_document(
-            """
-            Object.defineProperty(navigator, 'languages', {
-              get: () => ['en-US', 'en'],
-            });
-            """
-        )
-        await page.evaluate_on_new_document(
-            """
-            const newProto = navigator.__proto__;
-            delete newProto.webdriver;
-            """
-        )
-        return browser, context, page
+    p = await async_playwright().start()
+    browser = await p.chromium.launch(
+        headless=STEALTH_SETTINGS["headless"],  # Set headless mode
+        args=["--no-sandbox", "--disable-setuid-sandbox"],
+    )
+    context = await browser.new_context(
+        user_agent=USER_AGENT,
+        locale="de-DE",
+        geolocation={"longitude": -43.1729, "latitude": -22.9068},
+        permissions=["geolocation"],
+        record_video_dir="panties/",
+        record_video_size={"width": 1280, "height": 720},
+    )
+    page = await context.new_page()
+    await page.evaluate_on_new_document(
+        """
+        Object.defineProperty(navigator, 'webdriver', {
+          get: () => false,
+        });
+        """
+    )
+    await page.evaluate_on_new_document(
+        """
+        Object.defineProperty(navigator, 'languages', {
+          get: () => ['en-US', 'en'],
+        });
+        """
+    )
+    await page.evaluate_on_new_document(
+        """
+        const newProto = navigator.__proto__;
+        delete newProto.webdriver;
+        """
+    )
+    return p, browser, context, page
 
 
-# Function to navigate to a URL and check if it's loaded successfully
 async def navigate_and_check_url(page, url):
     response = await page.goto(url)
     if response.ok:
@@ -96,45 +94,40 @@ async def navigate_and_check_url(page, url):
     return response.ok
 
 
-# Function to scroll to the bottom of a page
 async def scroll_to_bottom(page):
+    # Scroll to bottom of page
     await page.evaluate("window.scrollTo(0, document.body.scrollHeight);")
 
 
-# Function to capture a screenshot
 async def capture_screenshot(page, url):
+    # Take a screenshot
     screenshot_name = url.split("/")[-2] + ".png"
     await page.screenshot(path=f"panties/{screenshot_name}")
-    logger.info(f"Screenshot saved as panties/{screenshot_name}")
+    logger.info(f"Screenshot saved as screenshots/{screenshot_name}")
 
 
-# Function to perform actions on a page
 async def perform_action_on_page(page, url):
-    try:
-        # Enter search query
-        await page.fill("input#searched-query", "ass")
-        await page.press("input#searched-query", "Enter")
-        logger.info("Search query entered: ass")
+    # Enter search query
+    await page.fill("input#searched-query", "ass")
+    await page.press("input#searched-query", "Enter")
+    logger.info("Search query entered: ass")
 
-        await scroll_to_bottom(page)
-        await capture_screenshot(page, url)
-        logger.info(f"Recording video to panties/{get_filename(url, 'webm')}")
-        await asyncio.sleep(5)  # Record for 5 seconds
-    except Exception as e:
-        logger.exception(f"An error occurred: {str(e)}")
+    await scroll_to_bottom(page)
+    await capture_screenshot(page, url)
+    logger.info(f"Recording video to panties/{get_filename(url, 'webm')}")
+    await asyncio.sleep(5)  # Record for 5 seconds
 
 
-# Main function
 async def s1s():
-    try:
-        browser, context, page = await setup_browser()
-        for url in URLS:
-            try:
-                if await navigate_and_check_url(page, url):
-                    await perform_action_on_page(page, url)
-            except Exception as e:
-                logger.exception(f"An error occurred: {str(e)}")
-    finally:
-        await context.close()
-        await browser.close()
+    p, browser, context, page = await setup_browser()
+    for url in URLS:
+        try:
+            if await navigate_and_check_url(page, url):
+                await perform_action_on_page(page, url)
+        except Exception as e:
+            logger.exception(f"An error occurred: {str(e)}")
+        finally:
+            await context.close()
+    await browser.close()
+    await p.stop()
     logger.info("All videos saved")

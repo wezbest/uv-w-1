501285ac6578d5a34adc30425a99ce8b35900f62
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-22 15:20:58 +0000

    🫦

--- TEZT/s5/src/w2/p3.py
+++ TEZT/s5/src/w2/p3.py
@@ -16,7 +16,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(message)s",
     datefmt="[%X]",
-    handlers=[RichHandler(rich_tracebacks=True)],
+    handlers=[RichHandler(rich_tracebacks=True, markup=True)],
 )
 log = logging.getLogger("rich")
 console = Console()

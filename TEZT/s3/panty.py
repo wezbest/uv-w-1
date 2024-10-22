f594a5da3981b6dc961611f88914b940e0c4f082
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-22 07:32:16 +0000

    🫦

---
+++ TEZT/s3/panty.py
@@ -0,0 +1,15 @@
+from rich.traceback import install
+from src.p1 import pussy
+
+install(show_locals=True)
+
+###############################
+
+
+def main():
+    print("Hello from s3!")
+    pussy()
+
+
+if __name__ == "__main__":
+    main()

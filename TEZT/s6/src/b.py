7a6b3b3a591f2efc51585f875e365b5bca906365
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-22 16:53:56 +0000

    🫦

--- TEZT/s6/src/b.py
+++ TEZT/s6/src/b.py
@@ -29,7 +29,7 @@ def pussy() -> None:
             r.raise_for_status()  # Raises an HTTPError for bad responses
             logger.info(f"Success: Status code {r.status_code}")
             print(r.text)  # This will print the response text to the console
-            console.rule(f"[bold green] {x} [/bold green]", style="bold green")
+            # console.rule(f"[bold green] {x} [/bold green]", style="bold green")
         except rq.RequestException as e:
             logger.error(
                 f"Failed to fetch {x}. Status code: {e.response.status_code if e.response else 'N/A'}. Error: {str(e)}"
